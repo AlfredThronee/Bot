09d37defa7d3619e90c4af81d598ad7eb59a9f1b
Alfred Thronée <71073054+AlfredThronee@users.noreply.github.com>
2021-03-17 12:14:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
     print('Alfreds skapelse lever!')
 
-client.run('hemlig token')
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+client.run('token')
