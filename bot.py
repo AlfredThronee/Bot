60525252b6dd9f20d9aae37175ecf2414337c7c7
Alfred Thronée <71073054+AlfredThronee@users.noreply.github.com>
2021-03-23 09:51:08 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,17 @@ client = commands.Bot(command_prefix = '.')
 @client.event
 async def on_ready():
     print('Alfreds skapelse lever!')
+    activity = discord.Activity(name='you all', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)
+
+@client.command()
+async def game(ctx):
+    channel = client.get_channel(823835795726925834)
+    await channel.send('gaming')
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    channel = client.get_channel(823835795726925834)
+    await channel.send(f'Pong! {round(client.latency * 1000)}ms')
 
 client.run('token')
